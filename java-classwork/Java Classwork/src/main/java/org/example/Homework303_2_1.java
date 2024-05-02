a6f89e958da669878da0964d93978c8022cf31ca
Anjaom <eric@webworkbench.net>
2024-05-02 08:11:36 -0600

    solved homework final problem

--- java-classwork/Java Classwork/src/main/java/org/example/Homework303_2_1.java
+++ java-classwork/Java Classwork/src/main/java/org/example/Homework303_2_1.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import java.text.DecimalFormat;
+
 public class Homework303_2_1 {
 
-    public static void main(String[] args){
+    static final double SALES_TAX = 0.09;
+
+
+    public static void main(String[] args) {
 
         // Write a program that declares two integer variables, assigns an integer to each, and adds them together.
         // Assign the sum to a variable. Print out the result.
@@ -35,7 +40,38 @@ public class Homework303_2_1 {
         int v1 = 5;
         int v2 = 2;
 
-        double div = (double)v1 / (double)v2;
+        double div = (double) v1 / (double) v2;
         System.out.println(div);
+
+
+        Integer abc = null;
+        abc = 150;
+
+        double abcd = abc.doubleValue();
+
+
+        // Write a program where you create three variables that represent products at a cafe. The products could be beverages like coffee,
+        // cappuccino, espresso, green tea, etc. Assign prices to each product. Create two more variables called subtotal and totalSale and
+        // complete an “order” for three items of the first product, four items of the second product, and two items of the third product.
+        // Add them all together to calculate the subtotal. Create a constant called SALES_TAX and add sales tax to the subtotal to obtain
+        // the totalSale amount. Be sure to format the results to two decimal places.
+
+        double coffee = 2.50;
+        coffee = 2.55;
+
+        double cappuccino = 3.50;
+        double espresso = 4.50;
+
+        double subtotal = (coffee * 3) + (cappuccino * 4) + (espresso * 2);
+
+        double salesTaxAmount = subtotal * SALES_TAX;
+        double totalAmount = subtotal + salesTaxAmount;
+
+        DecimalFormat df = new DecimalFormat("0.00");
+        System.out.println("Subtotal   : " + df.format(subtotal));
+        System.out.println("Sales Tax  : " + df.format(salesTaxAmount));
+        System.out.println("Total Sale : " + df.format(totalAmount));
+
+
     }
 }
