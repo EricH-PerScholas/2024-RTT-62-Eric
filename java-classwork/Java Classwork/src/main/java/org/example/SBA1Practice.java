679d0c9beb81704e5fa8dcfa0abba900d896e4be
Anjaom <eric@webworkbench.net>
2024-05-09 12:14:59 -0600

    SBA Practice Code

---
+++ java-classwork/Java Classwork/src/main/java/org/example/SBA1Practice.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class SBA1Practice {
+
+    public static void main(String[] args) {
+        String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        // part 1 - create a for loop to print each character of the string s on its own line
+        // you can use the s.charAt(pos) function
+
+        /*
+          A
+          B
+          C
+          D
+          E
+          F
+          G
+          ...
+         */
+
+        // part 2
+        // Given the following Strings convert them to integer values   Integer.valueOf()
+        String ten = "10";
+        String hundred = "100";
+
+        // part 3
+        // given an integer array find the sum of all the integers in the array
+        int[] array = {2, 4, 5, 6, 7, 8, 10};
+
+        // part 4
+        // given the array of string covert them to numbers and print the sum
+        String[] nums = {"2", "4", "5", "6", "7", "8", "10"};
+    }
+
+
+}
