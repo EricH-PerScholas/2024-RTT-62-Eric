60533420bd201c8f6fef7b2437a539047920628e
Anjaom <eric@webworkbench.net>
2024-05-09 13:14:57 -0600

    answers for SBA practice

--- java-classwork/Java Classwork/src/main/java/org/example/SBA1Practice.java
+++ java-classwork/Java Classwork/src/main/java/org/example/SBA1Practice.java
@@ -7,6 +7,9 @@ public class SBA1Practice {
 
         // part 1 - create a for loop to print each character of the string s on its own line
         // you can use the s.charAt(pos) function
+        for ( int pos = 0 ; pos < s.length() ; pos++) {
+            System.out.println(s.charAt(pos));
+        }
 
         /*
           A
@@ -24,13 +27,27 @@ public class SBA1Practice {
         String ten = "10";
         String hundred = "100";
 
+        int ten1 = Integer.valueOf(ten);
+        int hun = Integer.valueOf(hundred);
+
         // part 3
         // given an integer array find the sum of all the integers in the array
         int[] array = {2, 4, 5, 6, 7, 8, 10};
+        int sum = 0;
+        for ( int pos = 0 ; pos < array.length ; pos++) {
+            sum = sum + array[pos];
+        }
+        System.out.println(sum);
 
         // part 4
         // given the array of string covert them to numbers and print the sum
         String[] nums = {"2", "4", "5", "6", "7", "8", "10"};
+        int sum1 = 0;
+        for ( String string : nums ) {
+            int value = Integer.valueOf(string);
+            sum1 = sum1 + value;
+        }
+        System.out.println(sum1);
     }
 
 
