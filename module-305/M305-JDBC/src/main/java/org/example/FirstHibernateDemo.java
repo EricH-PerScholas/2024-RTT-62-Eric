99c4d0aa3aadd9e5a28c13bbfb3badbb0fe0de82
Anjaom <eric@webworkbench.net>
2024-06-04 13:25:23 -0600

    added findById and some boilerplate update and delete code

--- module-305/M305-JDBC/src/main/java/org/example/FirstHibernateDemo.java
+++ module-305/M305-JDBC/src/main/java/org/example/FirstHibernateDemo.java
@@ -11,25 +11,34 @@ public class FirstHibernateDemo {
         EmployeeDAO employeeDAO = new EmployeeDAO();
 
         // lets create a new employee in the database
-        Employee newEmployee = new Employee();
+//        Employee newEmployee = new Employee();
+//
+//        newEmployee.setFirstname("First NAME!!!");
+//        newEmployee.setLastname("Lastname");
+//        newEmployee.setEmail("rtt6211@perschols.org");
+//        newEmployee.setOfficeId(3);
+//        newEmployee.setExtension("X12345");
+//        newEmployee.setJobTitle("Newbie");
+//        newEmployee.setVacationHours(0);
+//
+//        employeeDAO.insert(newEmployee);
 
-        newEmployee.setFirstname("First NAME!!!");
-        newEmployee.setLastname("Lastname");
-        newEmployee.setEmail("rtt6211@perschols.org");
-        newEmployee.setOfficeId(3);
-        newEmployee.setExtension("X12345");
-        newEmployee.setJobTitle("Newbie");
-        newEmployee.setVacationHours(0);
+//        List<Employee> employees = employeeDAO.findByFirstName("First NAME!!!");
+//        //List<Employee> employees = employeeDAO.findByLastName("Doe");
+//
+//        for ( Employee employee : employees ) {
+//            System.out.println(employee);
+//        }
 
-        employeeDAO.insert(newEmployee);
+        // this employee id exists in the database and will return a record
+        Employee employee = employeeDAO.findById(1056);
+        System.out.println(employee);
 
-        List<Employee> employees = employeeDAO.findByFirstName("First NAME!!!");
-        //List<Employee> employees = employeeDAO.findByLastName("Doe");
+        // this employee id does not exist in the database and will return null
+        // you know your query found a result if it is not null
+        employee = employeeDAO.findById(1);
+        System.out.println(employee);
 
-        for ( Employee employee : employees ) {
-            System.out.println(employee);
-        }
-        
     }
 
 }
--- module-305/M305-JDBC/src/main/java/org/example/database/dao/EmployeeDAO.java
+++ module-305/M305-JDBC/src/main/java/org/example/database/dao/EmployeeDAO.java
@@ -29,6 +29,64 @@ public class EmployeeDAO {
         session.close();
     }
 
+    public void update(Employee employee) {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        session.getTransaction().begin();
+
+        // this is the only line that changed
+        session.merge(employee);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete(Employee employee) {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        session.getTransaction().begin();
+
+        // this is the only line that changed
+        session.delete(employee);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+
+    public Employee findById(Integer id) {
+        // these 2 lines of code prepare the hibernate session for use
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+
+        // JPA Query - the syntax is slightly different than regular SQL
+        String hql = "SELECT e FROM Employee e where e.id = :id";
+
+        // this typed query is how hibernate knows what kind of object of fill up with the query results
+        TypedQuery<Employee> query = session.createQuery(hql,Employee.class);
+
+        // this is similar to the prepared statement, we are going to set the value in the query :firstname
+        // to the value passed into the function
+        query.setParameter("id", id);
+
+        // we know we are querying a PK so the result will either be found or not found
+        // unfortunately, in 2010 this implementation throws an exception if the record is not found
+        // this does not exist in 2024
+        try {
+            Employee result = query.getSingleResult();
+            return result;
+        } catch ( Exception e ) {
+            return null;
+        } finally {
+            // finally we close the hibernate session so it can release the resources its holding
+            // specifically the connection pool and close the transaction
+            session.close();
+        }
+
+    }
+
+
+
     public List<Employee> findByFirstName(String firstName) {
         // these 2 lines of code prepare the hibernate session for use
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
