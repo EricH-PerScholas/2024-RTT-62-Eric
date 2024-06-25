af8bcb2005196e4fe164f24f3b5fd04a6524dc27
Anjaom <eric@webworkbench.net>
2024-06-25 09:41:01 -0600

    javascript event listeners

---
+++ module-308/class-examples/buttonclick.js
@@ -0,0 +1,8 @@
+
+function changeText() {
+    const p = document.querySelector('#discordConfigPTag');
+    p.textContent = "Changed because of an event handler property.";
+    
+    const button = document.querySelector('button');
+    button.textContent = "I was clicked";
+}
