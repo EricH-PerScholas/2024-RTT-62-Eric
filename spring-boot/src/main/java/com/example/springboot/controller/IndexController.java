6e3e47f7ddd95f8c0e201af61eb0fd81cab76557
Anjaom <eric@webworkbench.net>
2024-07-11 12:06:44 -0600

    added path variable to the index controller

--- spring-boot/src/main/java/com/example/springboot/controller/IndexController.java
+++ spring-boot/src/main/java/com/example/springboot/controller/IndexController.java
@@ -6,10 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -20,9 +17,11 @@ public class IndexController {
     private ProductDAO productDao;
 
 
+    // this function is for the home page of the website which is express as just a plain slash "/"
     @GetMapping("/")
-    public ModelAndView index(@RequestParam Integer id) {
-        // this function is for the home page of the website which is express as just a plain slash "/"
+    public ModelAndView index(@RequestParam(required = false) Integer id) {
+        // by default, the @RequestParam is required, meaning if it is missing in the URL spring will send you to the error page
+        // if the id parameter is not present on the URL then the incoming id will be null
         ModelAndView response = new ModelAndView("index");
 
         Product product = productDao.findById(id);
@@ -33,6 +32,22 @@ public class IndexController {
 
         return response;
     }
+
+    @GetMapping("/index/{id}")
+    public ModelAndView indexPathVar(@PathVariable(name = "id") Integer productId) {
+        // by default, the @PathVariable is required, meaning if it is missing in the URL spring will send you to the error page
+        // by using the name attribute in the @PathVariable annotation, you can change the name of the variable
+        // so in this case it is mapping the id in the URL to the productId variable
+        ModelAndView response = new ModelAndView("index");
+
+        Product product = productDao.findById(productId);
+        response.addObject("productKey", product);
+
+        response.addObject("message", "Hello World!");
+
+        return response;
+    }
+
 
     @GetMapping("/page-url")
     public ModelAndView canBeAnyFunctionNameYouWant() {
