0801ca15811ccdeb7b1a87ee5ce17fa59f281751
Anjaom <eric@webworkbench.net>
2024-07-12 13:21:07 -0600

    added some new controllers

---
+++ spring-boot/src/main/java/com/example/springboot/controller/CustomerController.java
@@ -0,0 +1,23 @@
+package com.example.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+    // this URL is going to be localhost:8080/customer/list
+    @GetMapping("/search")
+    public ModelAndView list() {
+       return null;
+    }
+
+    @GetMapping("/detail")
+    public ModelAndView detail() {
+       return null;
+    }
+
+}
---
+++ spring-boot/src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.example.springboot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+}
