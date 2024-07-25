cf47f843f08c2da746f8220f7604952030b77e06
Anjaom <eric@webworkbench.net>
2024-07-25 13:20:59 -0600

    comments for otther parts of spring data jpa

--- spring-boot/src/main/java/com/example/springboot/database/dao/CustomerDAO.java
+++ spring-boot/src/main/java/com/example/springboot/database/dao/CustomerDAO.java
@@ -2,13 +2,18 @@ package com.example.springboot.database.dao;
 
 import com.example.springboot.database.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.*;
 
 import java.util.List;
 
-
+@Repository
 public interface CustomerDAO extends JpaRepository<Customer, Long> {
 
     Customer findById(Integer id);
 
     List<Customer> findBySalesRepEmployeeId(Integer id);
+
+    int countBySalesRepEmployeeId(Integer id);
+
+    boolean existsBySalesRepEmployeeId(Integer id);
 }
